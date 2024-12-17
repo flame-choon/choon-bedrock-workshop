8f3e462b78e824eb6c0fcd6e2ad0596cb5d14d32
kwangjin.hwang <kwangjin.hwang>
2024-12-17 11:15:36 +0900

    update preequisites

---
+++ 00_Prerequisites/README.md
@@ -0,0 +1,8 @@
+# 0 - Prerequisites
+
+aws bedrock 의 FM (Foundation Model) 에 질의를 하고 응답을 받기 위해서는 AWS에서 제공하는 API 을 통해 할 수 있다.
+API를 이용하여 질의하고 응답 받기 위해선 다양한 방법을 이용할 수 있다.
+
+
+
+
---
+++ 00_Prerequisites/bedrock_basic.py
@@ -0,0 +1,87 @@
+import json
+import os
+import sys
+
+import boto3
+import botocore
+
+# Set up the session
+session = boto3.Session(profile_name='choon')
+sts_client = session.client('sts')
+bedrock_assume_role = sts_client.assume_role(
+    RoleArn="arn:aws:iam::975049940746:role/starbucks-bedrock-assume-role",
+    RoleSessionName="bedrock-session"
+)
+
+# Create the Bedrock client
+bedrock_client = boto3.client('bedrock', 
+    region_name='us-east-1',
+    aws_access_key_id=bedrock_assume_role['Credentials']['AccessKeyId'],
+    aws_secret_access_key=bedrock_assume_role['Credentials']['SecretAccessKey'],
+    aws_session_token=bedrock_assume_role['Credentials']['SessionToken']
+)
+bedrock_runtime = boto3.client('bedrock-runtime',
+    region_name='us-east-1',
+    aws_access_key_id=bedrock_assume_role['Credentials']['AccessKeyId'],
+    aws_secret_access_key=bedrock_assume_role['Credentials']['SecretAccessKey'],
+    aws_session_token=bedrock_assume_role['Credentials']['SessionToken']
+)
+
+def invoke_bedrock_model(bedrock_runtime, 
+                         prompt_text, 
+                         model_id="amazon.titan-text-express-v1", 
+                         max_tokens=1024, 
+                         top_p=0.95, 
+                         temperature=0.2):
+    """
+    Invoke AWS Bedrock model with given parameters
+    
+    Args:
+        bedrock_runtime: Bedrock runtime client
+        prompt_text: Input text prompt
+        model_id: Bedrock model ID
+        max_tokens: Maximum number of tokens to generate
+        top_p: Top P sampling parameter
+        temperature: Temperature parameter for text generation
+    
+    Returns:
+        str: Generated text response
+    """
+    try:
+        body = json.dumps({
+            "inputText": prompt_text,
+            "textGenerationConfig": {
+                "maxTokenCount": max_tokens,
+                "topP": top_p,
+                "temperature": temperature
+            }
+        })
+        
+        response = bedrock_runtime.invoke_model(
+            body=body,
+            modelId=model_id,
+            accept="application/json",
+            contentType="application/json"
+        )
+        response_body = json.loads(response.get("body").read())
+        return response_body.get("results")[0].get("outputText")
+
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'AccessDeniedException':
+            print(f"\x1b[41m{error.response['Error']['Message']}\
+                \nTo troubeshoot this issue please refer to the following resources.\
+                \nhttps://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_access-denied.html\
+                \nhttps://docs.aws.amazon.com/bedrock/latest/userguide/security-iam.html\x1b[0m\n")
+        else:
+            raise error
+        return None
+
+# Usage example
+prompt_data = """Command: Write me a blog about making strong business decisions as a leader.
+
+Blog:
+"""
+
+response_text = invoke_bedrock_model(bedrock_runtime, prompt_data)
+if response_text:
+    print(response_text)
